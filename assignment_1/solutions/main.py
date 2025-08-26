257a56268bcfb49482dbee3a04a69f6b418078e7
jonatanberge <jonatanberge@gmail.com>
2025-08-26 12:50:30 +0200

    Assignment 1

--- assignment_1/solutions/main.py
+++ assignment_1/solutions/main.py
@@ -3,13 +3,15 @@ import cv2
 
 image = cv2.imread('lena.png')
 
-
+# Task IV
 def print_image_information(image):
 
     height, width, channels = image.shape
 
     print("Height:", height)
+
     print("Width:", width)
+    
     print("Channels:", channels)
 
     print("Size:", image.size)
@@ -21,6 +23,19 @@ image = cv2.imread('lena.png')
 
 print_image_information(image)
 
-#cv2.imshow('image',image)
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
+# Task V
+
+cam = cv2.VideoCapture(0)
+
+fps = cam.get(cv2.CAP_PROP_FPS)
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
+with open('camera_outputs.txt', 'w') as f:
+    f.write(f"Width: {frame_width}\n")
+    f.write(f"Height: {frame_height}\n")
+    f.write(f"FPS: {fps}\n")
+
+
+cam.release()
