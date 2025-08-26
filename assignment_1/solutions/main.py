87837d740408413618d2a8697ad3c06a4cb10fe9
jonatanberge <jonatanberge@gmail.com>
2025-08-26 12:22:49 +0200

    initial commit

---
+++ assignment_1/solutions/main.py
@@ -0,0 +1,26 @@
+﻿import numpy as np
+import cv2
+
+image = cv2.imread('lena.png')
+
+
+def print_image_information(image):
+
+    height, width, channels = image.shape
+
+    print("Height:", height)
+    print("Width:", width)
+    print("Channels:", channels)
+
+    print("Size:", image.size)
+
+    print("Data type:", image.dtype)
+
+
+image = cv2.imread('lena.png')
+
+print_image_information(image)
+
+#cv2.imshow('image',image)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
